c97f9e6b5d9da0f7bd5a4709637ad8c6c08732a7
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2019-01-01 14:50:24 -0800

    deleted double

--- squarenumber.js
+++
@@ -1,9 +0,0 @@
-var isSquare = function (n) {
-  if (Number.isInteger(Math.sqrt(n))) {
-    return true
-  } else {
-    return false
-  }
-}
-console.log(isSquare(25));
-// console.log(Math.sqrt(3));
